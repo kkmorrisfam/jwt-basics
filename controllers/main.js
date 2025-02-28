418cabdfe8831085770c323fb6bb72c260f38ade
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-27 20:27:39 -0800

    set up baskc routes to test

---
+++ controllers/main.js
@@ -0,0 +1,24 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front-end
+
+// setup authentication so only the reques with JWT can access the dashboard
+
+const login = async (req, res) => {
+  res.send("Fake Login/Register/Signup Route");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100)
+  res
+    .status(200)
+    .json({
+      msg: `Hello, John Doe`,
+      secret: `Here it is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
