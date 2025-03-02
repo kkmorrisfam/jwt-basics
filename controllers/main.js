01a20e33fc3d3882c687d1b00b5b93ce896969ee
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-03-01 20:12:10 -0800

    sign token, test token

--- controllers/main.js
+++ controllers/main.js
@@ -3,19 +3,55 @@
 // send back to front-end
 
 // setup authentication so only the reques with JWT can access the dashboard
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+// require("dotenv").config();
 
 const login = async (req, res) => {
-  res.send("Fake Login/Register/Signup Route");
+  const { username, password } = req.body;
+  // console.log(`${username} ${password}`)
+  // validation options:
+  // mongoose validation
+  // Joi  package, use later
+  // check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
+  //for testing/demo, normally provided by DB
+  const id = new Date().getDate;
+  //   console.log(id);
+  //only provide resources that belong to user
+  //don't use password in payload
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+try {
+    const decoded = jwt.verify(token process.env.JWT_SECRET)
+} catch (error) {
+    throw new CustomAPIError('Not authorized to access this route', 401)
+}
+
+//   console.log(token);
+  //   console.log(username, password);
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100)
-  res
-    .status(200)
-    .json({
-      msg: `Hello, John Doe`,
-      secret: `Here it is your authorized data, your lucky number is ${luckyNumber}`,
-    });
+  console.log(req.headers);
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, John Doe`,
+    secret: `Here it is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
